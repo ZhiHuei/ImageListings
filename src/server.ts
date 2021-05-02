a6ea739bae304e1e09b8568fa9f5dff5cf108e75
zh <zhihuei94@gmail.com>
2021-05-02 18:12:59 +0800

    loaded some images

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
     graphqlExpress
   } from 'graphql-server-express';
 import { schema } from './graphql/schema';
+import * as config from '../public/config.json';
 
 export class Server {
     private app;
@@ -22,6 +23,38 @@ export class Server {
     public start = async (port: number) => {
         console.log("starting.....");
         await DataBaseConnection.init();
+
+        // read album and load into db
+        const fs = require('fs');
+        fs.readdir(config.dev.repo, (err: any, files: any[]) => {
+            if (err)
+                throw new Error(err);
+
+            files.forEach(category => {
+                fs.lstat(config.dev.repo + '\\' + category, async (err: any, stats: any) => {
+                    if (err)
+                        throw new Error(err);
+
+                    // Catogorised images
+                    if (stats.isDirectory()) {
+                        fs.readdir(config.dev.repo + '\\' + category, (err: any, files: any[]) => {
+                            if (err)
+                                throw new Error(err);
+
+                            files.forEach(async image => {
+                                await DataBaseConnection.findOrAddImage(image.split('.').slice(0, -1).join('.'), category);
+                            });
+                            console.log(`${files.length} loaded from ${category}`);
+                        });
+                    }
+                    // Uncategorised images
+                    else {
+                        await DataBaseConnection.findOrAddImage(category.split('.').slice(0, -1).join('.'), 'Uncategorized');
+                    }
+
+                });
+            });
+        });
 
         return new Promise((resolve, reject) => {
             this.app.listen(port, () => {
